fca892f777e72dd610a93af17a041b656bdc5d5c
Alexander Messier <alexander.j.messier@gmail.com>
2021-10-08 14:40:58 -0500

    Changed release_date to a DateField.

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -8,5 +8,5 @@ class Song(models.Model):
     album = models.CharField(max_length=50)
     artist = models.CharField(max_length=50) 
     genre = models.CharField(max_length=50)
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
     likes = models.IntegerField(default=0)
